d3e61bb9217e01901d78ad2cbdace9bcf936634f
Sam <schristy@medford.k12.ma.us>
2023-01-17 14:49:26 -0500

    working sender info

--- text-type.py
+++ text-type.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import Message, MessagingResponse
 from datetime import datetime
-#from pandas import read_csv
+from pandas import read_csv
 from serial import Serial
-#import serial
 import atexit
-#import csv
 
 port = Serial('/dev/ttyACM0', 9600)
 
@@ -24,17 +22,24 @@ def sms():
     #current_time = now.strftime("%D %I:%M%p ")
     current_time = now.strftime("%I:%M%p ")
 
-
-    number = request.form['From']
+    message_number = request.form['From']
     message_body = request.form['Body']
 
-    #numbers = read_csv('phone_numbers.csv')
+    numbers = read_csv('phone_numbers.csv')
+    numbers = numbers.set_index('number').to_dict()['name']
+    message_name = numbers.get(message_number)
 
-    #from IPython import embed; embed()
-    #numbers = numbers.set_index('number').to_dict()['name']
+    if not bool(message_name):
+        sender = message_number + ': '
+    else:
+        sender = message_name + ': '
+
     LINE_WIDTH = 70
 
-    text_to_write = current_time + message_body
+    text_to_write = current_time + sender + message_body
+
+    if len(text_to_write) > 190:
+        text_to_write = text_to_write[:190]
    
     while(text_to_write):
         port.write(text_to_write[:LINE_WIDTH].encode())
@@ -43,12 +48,6 @@ def sms():
             port.write(b'-')
         port.write(b'\r')
 
-    print(current_time)
-    print(number)
-    print(message_body)
-   # with open('numbers.csv', mode='a') as csv_file:
-   #@     writer = csv.writer(csv_file)
-    #    writer.writerow(number)
     return message_body
 
 if __name__ == '__main__':
